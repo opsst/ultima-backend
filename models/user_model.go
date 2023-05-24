f37eff2a86ff48d3886533d6852381525712698f
Nattapat Sittichai <73817577+Voidntpx@users.noreply.github.com>
2023-05-25 00:06:46 +0700

    Add profile_img model

--- models/user_model.go
+++ models/user_model.go
@@ -6,6 +6,7 @@ type User struct {
 	Id            primitive.ObjectID `json:"id,omitempty"`
 	Email         string             `json:"email,omitempty"`
 	Password      string             `json:"password,omitempty"`
+	Profile_img   string             `json:"profile_img,omitempty"`
 	Firstname     string             `json:"firstname,omitempty"`
 	Lastname      string             `json:"lastname,omitempty"`
 	Admin         string             `json:"admin,omitempty"`
